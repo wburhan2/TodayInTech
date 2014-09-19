36321d942c35d637e84dde5ef3c7d412c5d05d4e
Wilson Burhan <wilson.burhan@gmail.com>
2014-09-19 00:26:45 -0700

    added universal image loader library and added url drawable image parser.

---
+++ app/src/main/java/com/wilsonburhan/todayintech/TodayInTechActivity.java
@@ -0,0 +1,69 @@
+package com.wilsonburhan.todayintech;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.wilsonburhan.todayintech.service.NewsList;
+
+
+public class TodayInTechActivity extends FragmentActivity implements NewsList.OnArticleSelectedListener, NewsList.OnRefreshArticlesListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        if (findViewById(R.id.fragment_container) != null) {
+
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            NewsList smallScreenFragement = new NewsList();
+            smallScreenFragement.setArguments(getIntent().getExtras());
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, smallScreenFragement)
+                    .commit();
+        }
+
+        //updateStories();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.today_in_tech, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onArticleSelected(long _id) {
+
+    }
+
+    @Override
+    public void onSelectDefault(long _id) {
+
+    }
+
+    @Override
+    public void onRefreshArticles() {
+
+    }
+}
