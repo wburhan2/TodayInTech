873ea8a9434ac7e2db4a8882908db6e4adf66cfb
Wilson Burhan <wilson.burhan@gmail.com>
2014-09-21 00:10:07 -0700

    Added support to RSS 2.0

--- app/src/main/java/com/wilsonburhan/todayintech/service/TodayInTechGetRss.java
+++ app/src/main/java/com/wilsonburhan/todayintech/service/TodayInTechGetRss.java
@@ -14,8 +14,8 @@ import android.util.Xml;
 import com.wilsonburhan.todayintech.TodayInTechContract;
 
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.w3c.dom.*;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 
@@ -25,13 +25,18 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 /**
  * Created by Wilson on 9/20/2014.
  */
 public class TodayInTechGetRss {
     private static URL mUrl = null;
-    private static final String RSS_FEED_URL = "http://podcasts.engadget.com/rss.xml";
+    private static final String RSS_FEED_URL = "http://www.huffingtonpost.com/feeds/verticals/technology/index.xml";
     private static final String ATOM = "http://www.w3.org/2005/Atom";
+    private static final String PURL = "http://purl.org/dc/elements/1.1/";
+    private static final String ITUNES = "http://www.itunes.com/dtds/podcast-1.0.dtd";
 
 
     public static void get(ContentResolver contentResolver) {
@@ -40,14 +45,108 @@ public class TodayInTechGetRss {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
     private static void getXML(ContentResolver resolver) throws IOException, SAXException {
-
         mUrl = new URL(RSS_FEED_URL);
         final ValueList items = new ValueList();
         InputStream stream = (InputStream) mUrl.getContent();
+
+        String rootName = getRootNode(stream);
+
+        if (rootName.equals("feed"))
+            atomParser(stream, items, resolver);
+        else if (rootName.equals("rss")){
+            rssParser(stream, items, resolver);
+        }
+
+    }
+
+    private static void rssParser(InputStream stream, final ValueList items, ContentResolver resolver) throws IOException, SAXException{
+        RootElement root = new RootElement("rss");
+        android.sax.Element channel = root.getChild("channel");
+        // End of each entry add the entire row, and create a new row.
+        channel.getChild("item").setEndElementListener(new EndElementListener() {
+            @Override
+            public void end() {
+                items.rows.add(items.currentRow);
+                items.currentRow = new ContentValues();
+            }
+        });
+
+        // Add the article title
+        channel.getChild("item").getChild("title").setEndTextElementListener(new EndTextElementListener() {
+            @Override
+            public void end(String body) {
+                items.currentRow.put(TodayInTechContract.COLUMN_TITLE, body);
+            }
+        });
+
+        // Get the link to the article
+        channel.getChild("item").getChild("link").setStartElementListener(new StartElementListener() {
+            @Override
+            public void start(Attributes attributes) {
+                items.currentRow.put(TodayInTechContract.COLUMN_ARTICLE_LINK, attributes.getValue("href"));
+            }
+        });
+
+        // Add the article ID
+        channel.getChild("item").getChild("guid").setEndTextElementListener(new EndTextElementListener() {
+            @Override
+            public void end(String body) {
+                items.currentRow.put(TodayInTechContract.COLUMN_ARTICLE_ID, body);
+            }
+
+        });
+
+        // Add the Published Date
+        channel.getChild("item").getChild("pubDate").setEndTextElementListener(new EndTextElementListener() {
+            @Override
+            public void end(String body) {
+                items.currentRow.put(TodayInTechContract.COLUMN_PUBLISHED_DATE, body);
+            }
+
+        });
+
+        // Add the Author Name
+        channel.getChild("item").getChild("author").setEndTextElementListener(new EndTextElementListener() {
+            @Override
+            public void end(String body) {
+                items.currentRow.put(TodayInTechContract.COLUMN_AUTHOR_NAME, body);
+            }
+
+        });
+
+        // Add the Content
+        channel.getChild("item").getChild("description").setEndTextElementListener(new EndTextElementListener() {
+            @Override
+            public void end(String body) {
+                org.jsoup.nodes.Document doc = Jsoup.parse(body);
+                Element png = doc.select("img").first();
+                if (png != null) {
+                    Bitmap pic = getBitmapFromURL(png.absUrl("src"));
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    pic.compress(Bitmap.CompressFormat.PNG, 100, bos);
+                    items.currentRow.put(TodayInTechContract.COLUMN_PICTURE, bos.toByteArray());
+                    items.currentRow.put(TodayInTechContract.COLUMN_CONTENT, body.replaceAll("<img.+/(img)*>", ""));
+                }
+                else {
+                    items.currentRow.put(TodayInTechContract.COLUMN_CONTENT, body);
+                }
+            }
+
+        });
+
+       	/*
+       	 * Bulk insert just loops through the list one at a time, not that fast,
+       	 * could use an InsertHelper to speed it up, and direct DB write vs via the
+       	 * content resolver.
+       	 */
+        Xml.parse(mUrl.openStream(), Xml.Encoding.UTF_8, root.getContentHandler());
+        resolver.bulkInsert(TodayInTechContract.RSS_FEED_URI, items.getRows());
+    }
+
+    private static void atomParser(InputStream stream, final ValueList items, ContentResolver resolver) throws IOException, SAXException{
         RootElement root = new RootElement(ATOM, "feed");
 
         // End of each entry add the entire row, and create a new row.
@@ -115,7 +214,7 @@ public class TodayInTechGetRss {
         root.getChild(ATOM, "entry").getChild(ATOM, "content").setEndTextElementListener(new EndTextElementListener() {
             @Override
             public void end(String body) {
-                Document doc = Jsoup.parse(body);
+                org.jsoup.nodes.Document doc = Jsoup.parse(body);
                 Element png = doc.select("img").first();
                 if (png != null) {
                     Bitmap pic = getBitmapFromURL(png.absUrl("src"));
@@ -131,23 +230,13 @@ public class TodayInTechGetRss {
 
         });
 
-        // Add the Picture
-      /*  root.getChild(ATOM, "entry").getChild(ATOM, "content").getChild(ATOM, "img").setEndTextElementListener(new EndTextElementListener() {
-            @Override
-            public void end(String body) {
-                items.currentRow.put(TodayInTechContract.COLUMN_PICTURE_URI, body);
-            }
-
-        });*/
-
        	/*
        	 * Bulk insert just loops through the list one at a time, not that fast,
        	 * could use an InsertHelper to speed it up, and direct DB write vs via the
        	 * content resolver.
        	 */
-        Xml.parse(stream, Xml.Encoding.UTF_8, root.getContentHandler());
+        Xml.parse(mUrl.openStream(), Xml.Encoding.UTF_8, root.getContentHandler());
         resolver.bulkInsert(TodayInTechContract.RSS_FEED_URI, items.getRows());
-
     }
 
     public static Bitmap getBitmapFromURL(String src) {
@@ -166,5 +255,22 @@ public class TodayInTechGetRss {
             Log.e("Exception",e.getMessage());
             return null;
         }
+    }
+
+    private static String getRootNode(InputStream stream) {
+        Document doc = null;
+        try {
+            DocumentBuilderFactory fact = DocumentBuilderFactory.newInstance();
+            fact.setValidating(false);
+            fact.setNamespaceAware(false);
+            DocumentBuilder builder = fact.newDocumentBuilder();
+            doc = builder.parse(stream);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("Exception",e.getMessage());
+            return null;
+        }
+        Node node = doc.getDocumentElement();
+        return node.getNodeName();
     }
 }
